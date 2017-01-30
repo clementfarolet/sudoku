1306f42aa7295e27129a959b78eec03c5294e6c5
Clément <Clment@10.5.3.28>
2017-01-30 16:57:31 +0100

    grille affiché

---
+++ projet/src/projet/Bouton.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet;
+
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Clément
+ */
+public class Bouton extends JButton implements ActionListener {
+    
+    public void actionPerformed(ActionEvent e) {
+       ajouter();
+    }
+    
+    public void ajouter(){
+        
+    }
+    
+}
